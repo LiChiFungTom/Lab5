915166547293ab7cb1055045d9a0a840a0d2a08b
LiChiFungTom <72788072+LiChiFungTom@users.noreply.github.com>
2021-10-22 15:18:29 +0800

    Add files via upload

---
+++ Lab5/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    Command[] com = new Command[4];
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter command: 0 = exit , 1 = undo , 2 = Command1 , 3 = Command2");
+        int command = sc.nextInt(); 
+
+        
+        
+    }
+}
